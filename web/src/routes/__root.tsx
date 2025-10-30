332233572e191d783aaa6a4d4ba300ab714be159
EricaReis <ericabeatrizdosreis@gmail.com>
2025-10-29 21:11:51 -0300

    feat: add api routes and front-end integration

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,7 +1,12 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
 
 const RootLayout = () => (
-    <Outlet />
+    <QueryClientProvider client={queryClient}>
+        <Outlet />
+    </QueryClientProvider>
 )
 
 export const Route = createRootRoute({ component: RootLayout })
